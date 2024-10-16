e4c63130ebb2896de193c8e320506fd017bf8ff1
Niksor <Salfordyoutube@ya.ru>
2024-10-16 05:59:00 +0300

    Поправлен base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/russian-a/cross-the-century/',
+  base: '/russian-across-the-century/',
   // base: "/",
 });
