73d6e4a872cc8b40cbbe88dbcf742c4cb6ac03eb
Niksor <146173983+NiksorFront@users.noreply.github.com>
2024-10-28 11:09:22 +0300

    Обновлённый подвал

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -14,99 +14,36 @@
 <template>
     <div class="w-full max-w-[1200px] mx-auto text-white">
         <div class="container mx-auto py-6 sm:py-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-            <!-- Логотипы Синхранизации-->
-            <!-- <div class="grid grid-cols-4 sm:grid-cols-2 m-4 gap-1 sm:max-w-[160px] sm:max-h-[125px] lg:col-start-1 lg:col-span-2 lg:row-start-1">
-                <a href="https://online.synchronize.ru/" target="_blank"><img :src="synchronization" alt="Синхронизация" class="w-20 object-contain invert" /></a>
-                <a href="https://sk.ru/" target="_blank"><img :src="skolkovo" alt="участник Хуёлково" class="w-[62px] object-contain ml-auto translate-x-2 sm:translate-x-0" /></a>
-                <div class="grid col-span-2 grid-cols-3 ml-auto gap-1 sm:w-full sm:ml-0">
-                    <a href="https://t.me/+gY7XBrs4xvQ0Njcy" target="_blank"><img :src="tgLogo" alt="телеграмчик" class="w-10 object-contain" /></a>
-                    <a href="https://vk.com/synchronize" target="_blank"><img :src="vkLogo" alt="вконтосик" class="w-10 object-contain" /></a>
-                    <a href="https://podcast.ru/1711936901" target="_blank"><img :src="podcastLogo" alt="скучный микро" class="w-10 object-contain" /></a>
-                </div>
-            </div> -->
-
+            
             <!--Логотипы-->
-            <!-- <div class="grid grid-cols-5 m-4 gap-1 sm:max-w-[160px] sm:max-h-[125px] lg:col-start-1 lg:col-span-2 lg:row-start-1 justify-between"> -->
-            <div class="flex m-4 gap-1 max-w-[250px] lg:col-start-1 lg:row-start-1 justify-between">
-                <!-- <a href="https://online.synchronize.ru/" target="_blank"><img :src="synchronization" alt="Синхронизация" class="w-20 object-contain invert" /></a>
-                <a href="https://sk.ru/" target="_blank"><img :src="skolkovo" alt="участник Хуёлково" class="w-[62px] object-contain ml-auto translate-x-2 sm:translate-x-0" /></a> -->
-                <!-- <div class="grid col-span-2 grid-cols-3 ml-auto gap-1 sm:w-full sm:ml-0"> -->
+            <div class="flex m-4 gap-1 max-w-[250px] justify-between">
                 <a href="https://t.me/alliancelegalcg" target="_blank"><img :src="tgLogo" alt="телеграмчик" class="w-10 object-contain" /></a>
                 <a href="https://vk.com/alliance_legal" target="_blank"><img :src="vkLogo" alt="вконтосик" class="w-10 object-contain" /></a>
                 <a href="https://dzen.ru/id/622b489016aad67be6cdbf35" target="_blank"><img :src="dzenLogo" alt="дзен" class="w-10 object-contain" /></a>
                 <a href="https://www.youtube.com/channel/UC_As5xBHOMjW4dPB1bQEThg/featuredhttps%3A" target="_blank"><img :src="youtubeLogo" alt="ютубчик" class="w-10 object-contain" /></a>
                 <a href="https://zakon.ru/Profile/Show/229640" target="_blank"><img :src="hourglassLogo" alt="песочные часы" class="w-10 object-contain" /></a>
-                <!-- </div> -->
             </div>
             
-            <!-- Форма подписки 
-            <div class="mb-5 w-hit h-full max-h-[280px] sm:max-w-[280px] bg-[#2a282f] rounded-2xl mx-4 sm:col-start-2 sm:row-start-2 lg:col-span-4 lg:col-start-7 lg:row-start-1">
-                <h5 class="helvetica-700 footer__text ml-5 mt-5 mb-3.5">ХОЧУ БЫТЬ В КУРСЕ ВСЕГО</h5>
-                <form class="space-y-4 flex flex-col w-fit mx-5">
-                    <input type="email" placeholder="Введите email" class="w-full h-12 p-2 border border-white bg-[#2a282f] rounded-xl"/>
-                    <div class="flex flex-wrap gap-2">
-                        <input type="checkbox" class="size-4 accent-[#2a282f]">
-                        <span class="w-[90%] helvetica-500 form__text text-gray-500 opacity-80 mt-0.5">Подтверждаю, что мне больше 18 лет, и заявляю, что согласен с обработкой моих персональных данных в соответствии с <a href="https://docs.google.com/document/d/1YMnZH72TBClUCrCouuZuYsddu57FEsM0/edit?pli=1" target="_blank" class="underline form__text text-gray-500 opacity-70">Политикой обработки персональных данных </a></span>
-                    </div>
-                    <button type="submit" class="w-full h-11 bg-white helvetica-500 text-black py-2 rounded-xl">Подписаться</button>
-                </form>
-            </div>-->
-
-            <!-- Ссылки на разделы -->
-            <div class="m-4 mt-4 space-y-3.5 sm:col-start-2 lg:col-start-4 lg:col-span-2 lg:mx-0 sm:row-start-1">
-                <a href="https://online.synchronize.ru/referal_lk" target="_blank" class="block underline decoration-2 helvetica-700 footer__text hover:opacity-75 hover:decoration-1 cursor-pointer transition-all">ПРИГЛАСИТЬ ДРУГА</a>
-                <a href="https://online.synchronize.ru/#catalogue" target="_blank" class="block underline decoration-2 helvetica-700 footer__text hover:opacity-75 hover:decoration-1 cursor-pointer transition-all">КАТАЛОГ КУРСОВ</a>
-                <a href="https://special.synchronize.ru/joinus" target="_blank" class="block underline decoration-2 helvetica-700 footer__text hover:opacity-75 hover:decoration-1 cursor-pointer transition-all">ВАКАНСИИ</a>
-            </div>
-
-            <!-- <div class="mx-4 my-0 space-y-2 col-start-1 row-start-1 col-span-2 hidden lg:block mt-[auto]">
-                <p href="tel:74993914639" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">+ 7(499) 39 146 39</p>
-                <a href="mailto:hello@synchronize.ru" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">hello@synchronize.ru</a>
-                <a href="https://t.me/SynchronizeSupportBot?start=link_nTOgfsLiCm" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Поддержка в Telegram</a>
-            </div> -->
-
-            <div class="mx-4 my-0 space-y-1.5 col-start-2 row-start-1 hidden lg:block mt-3">
-                    <a href="tel:74996780120" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">+7 499 678-01-20</a>
+            <!-- Контакты -->
+            <div class="mx-4 my-0 sm:row-start-2 lg:row-start-1 space-y-2 mt-3">
+                    <a href="tel:74996780120" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 transition-all">+7 499 678-01-20</a>
                     <a href="mailto:alcg@live.ru" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">alcg@live.ru</a>
                     <p class="block helvetica-500 footer__text transition-all">125375, Москва<br>ул. Тверская, дом 16, строение 1<br> БЦ «Галерея Актер», офис А-501</p>
             </div>
+
             <!-- Контакты 2-->
-            <div class="mx-4 my-0 space-y-1.5 col-start-2 row-start-2 hidden lg:block mt-0">
+            <div class="mx-4 my-0 sm:row-start-3 lg:row-start-1 space-y-2 mt-3">
                 <a href="tel:73432867053" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">+7 343 286-70-53</a>
                 <a href="mailto:alcg@live.ru" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">alcg@live.ru</a>
                 <p class="block helvetica-500 footer__text transition-all">620144, Екатеринбург<br>ул. Шейнкмана, дом 121, 3 этаж<br>БЦ «Антарес-бизнес»</p>
             </div>
 
-            <div class="m-4 my-0 flex flex-col gap-6 sm:col-start-1 sm:row-start-2 lg:col-start-3 lg:col-span-2 lg:row-start-1 lg:flex-col-reverse lg:mx-0">
-                <!-- Контакты -->
-                <div class="space-y-2 lg:hidden">
-                    <!-- <p href="tel:74993914639" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">+ 7(499) 39 146 39</p>
-                    <a href="mailto:hello@synchronize.ru" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">hello@synchronize.ru</a>
-                    <a href="https://t.me/SynchronizeSupportBot?start=link_nTOgfsLiCm" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Поддержка в Telegram</a>-->
-                    <a href="tel:74996780120" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">+7 499 678-01-20</a>
-                    <a href="mailto:alcg@live.ru" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">alcg@live.ru</a>
-                    <p class="block helvetica-500 footer__text transition-all">125375, Москва<br> ул. Тверская, дом 16, строение 1<br> БЦ «Галерея Актер», офис А-501</p>
-                </div>
-                <!-- Контакты 2-->
-                <div class="space-y-2 lg:hidden">
-                    <p href="tel:73432867053" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">+7 343 286-70-53</p>
-                    <a href="mailto:alcg@live.ru" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">alcg@live.ru</a>
-                    <p class="block helvetica-500 footer__text transition-all">620144, Екатеринбург<br>ул. Шейнкмана, дом 121, 3 этаж<br>БЦ «Антарес-бизнес»</p>
-                </div>
-                <!-- Для бизнеса -->
-                <div class="space-y-2">
-                    <h3 class="helvetica-500 text-xs opacity-50">ДЛЯ БИЗНЕСА</h3>
-                    <a href="https://online.synchronize.ru/b2b" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Корпоративное обучение</a>
-                    <a href="https://online.synchronize.ru/b2b/cert" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Сертификаты на курсы</a>
-                </div>
-
-                <!-- Спецпредложения -->
-                <div class="space-y-2">
-                    <h3 class="helvetica-500 text-xs opacity-50">СПЕЦПРЕДЛОЖЕНИЯ</h3>
-                    <a href="https://online.synchronize.ru/sub" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Подписка на все курсы</a>
-                    <a href="https://online.synchronize.ru/gifts" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Подарочные сертификаты</a>
-                </div>
+            <!-- Для бизнеса  -->
+            <div class="mx-4 space-y-2 mt-3">
+                <h3 class="helvetica-500 text-xs opacity-50">ДЛЯ БИЗНЕСА</h3>
+                <a href="https://al-cg.com/" target="_blank" class="block helvetica-500 footer__text hover:opacity-75 cursor-pointer transition-all">Юридическая защита</a>
             </div>
+
         </div>
 
         <div class="container w-fit sm:w-full h-28 lg:h-14 mt-5 mx-4 sm:mx-auto helvetica-500 text-[#81808f] flex flex-col justify-between border-y border-[#2a282f] lg:flex-row">
